de06ffd3a9ca182d015f90284575bf1012b86e15
erlage <erlage.inc@gmail.com>
2022-02-24 14:43:07 +0530

    add: postcss & cssnano

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,31 @@
 // rollup.config.js
 import typescript from "@rollup/plugin-typescript";
 
+// css
+
+import postcss from "rollup-plugin-postcss";
+
 export default {
     input: "src/proton.ts",
     output: {
         dir: "dist",
     },
-    plugins: [typescript()],
+    plugins: [
+        typescript(),
+        postcss({
+            extensions: [".css"],
+            plugins: [
+                require("cssnano")({
+                    preset: [
+                        "default",
+                        {
+                            discardComments: {
+                                removeAll: true,
+                            },
+                        },
+                    ],
+                }),
+            ],
+        }),
+    ],
 };
